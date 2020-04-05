1e127454c757810987b5c007e0803c36707e9072
Francis-Olivier Couture <francis-olivier_couture@hotmail.com>
2020-04-05 16:45:10 -0400

    Updates

--- precache-manifest.e9e91025f313b0a161167bd13185921f.js
+++ precache-manifest.e9e91025f313b0a161167bd13185921f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d70103076b786450a6ea7cc22c90e8f2",
+    "revision": "72f1a7a8a905888ee7d374e7f90ce9e1",
     "url": "/distributor/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/distributor/static/css/2.a3b9b7a2.chunk.css"
   },
   {
-    "revision": "d59bf7334428ad1691c5",
-    "url": "/distributor/static/css/main.cab1c5fc.chunk.css"
+    "revision": "2168b738c79c07289533",
+    "url": "/distributor/static/css/main.0d171b7d.chunk.css"
   },
   {
     "revision": "866822a8bef3436984d2",
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/distributor/static/js/2.b2bb11dc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d59bf7334428ad1691c5",
+    "revision": "2168b738c79c07289533",
     "url": "/distributor/static/js/main.47c1b57b.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/distributor/precache-manifest.dd6d2ffdd8515e0c0a900a5f08d0dd12.js"
+  "/distributor/precache-manifest.e9e91025f313b0a161167bd13185921f.js"
 );
 
 self.addEventListener('message', (event) => {
