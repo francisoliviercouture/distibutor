19fae1f59afaca93b6597665202453cdf3e7eab8
Francis-Olivier Couture <francis-olivier_couture@hotmail.com>
2020-04-05 11:54:35 -0400

    Updates

---
+++ precache-manifest.a763a916d23ff384855655f3598872b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e6f084be41346754a301ef66f8ce409",
+    "url": "/francisoliviercouture.github.io/distributor/index.html"
+  },
+  {
+    "revision": "087d482355a929d89a13",
+    "url": "/francisoliviercouture.github.io/distributor/static/css/main.f588f341.chunk.css"
+  },
+  {
+    "revision": "be089a404b04eeb34719",
+    "url": "/francisoliviercouture.github.io/distributor/static/js/2.7a4cbe6d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/francisoliviercouture.github.io/distributor/static/js/2.7a4cbe6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "087d482355a929d89a13",
+    "url": "/francisoliviercouture.github.io/distributor/static/js/main.ebde8058.chunk.js"
+  },
+  {
+    "revision": "a9bbedd4aa68d46f5a55",
+    "url": "/francisoliviercouture.github.io/distributor/static/js/runtime-main.91c489b7.js"
+  },
+  {
+    "revision": "947205fb38e5c9fa65731b464412c4e5",
+    "url": "/francisoliviercouture.github.io/distributor/static/media/ambigue.947205fb.png"
+  }
+]);
--- src/core/models/beer-list-item.ts
+++
@@ -1,5 +0,0 @@
-export interface BeerListItem {
-    ibu: number;
-    alcoolPercentage: number;
-    type: string;
-}
